8d33ea6d9ee6a10b6a7f4ea7f0584a086cc9b9f0
Shadowman771 <monkey771yay@gmail.com>
2017-07-06 00:02:32 -0700

    Remove unused imports

--- src/main/java/com/kingofchaos/supplementalthings/blocks/BlockExit.java
+++ src/main/java/com/kingofchaos/supplementalthings/blocks/BlockExit.java
@@ -1,24 +1,12 @@
 package com.kingofchaos.supplementalthings.blocks;
 
 import com.kingofchaos.supplementalthings.SupplementalThings;
-import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyDirection;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.Rotation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
-
-import static com.kingofchaos.supplementalthings.blocks.BlockCustomModel.PixelSize;
 
 public class BlockExit extends BlockCustomModel {
 
--- src/main/java/com/kingofchaos/supplementalthings/blocks/BlockSupplementalThingsMain.java
+++ src/main/java/com/kingofchaos/supplementalthings/blocks/BlockSupplementalThingsMain.java
@@ -2,19 +2,8 @@ package com.kingofchaos.supplementalthings.blocks;
 
 import com.kingofchaos.supplementalthings.SupplementalThings;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyDirection;
-import net.minecraft.block.state.BlockStateContainer;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.Rotation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 
 public class BlockSupplementalThingsMain extends Block
 {
